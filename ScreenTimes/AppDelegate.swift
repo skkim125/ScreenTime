64237e6a6377d4ad6a9b1f612b04a38c00695cb6
knockknock <yangban11111@gmail.com>
2024-10-08 23:02:43 +0900

    [Add] IQKeyboard 설정

--- ScreenTimes/AppDelegate.swift
+++ ScreenTimes/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        IQKeyboardManager.shared.enable = true
+        //IQKeyboardManager.shared.enableAutoToolbar = false // deprecated되었다고 경고 문구가 나와서 주석처리 하였습니다
+        IQKeyboardManager.shared.resignOnTouchOutside = true
         return true
     }
 
