01dd69eda72fcf12f3f81466ff79fd3c7f2fe7ee
skkim125 <kthanks125@gmail.com>
2024-10-08 18:06:55 +0900

    [Chore] 프로젝트 기본 세팅

---
+++ ScreenTimes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenTimes
+//
+//  Created by 김상규 on 10/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
