0396713109b9ba26f3b871eb9de4901dac9617f8
knockknock <yangban11111@gmail.com>
2024-10-09 14:09:06 +0900

    [Feat] API 초기 설정 및 API 연결
    - NetworkManager 생성
    - TrendingMovie model 생성
    - TrendingMovie API 연결

--- ScreenTimes/Network/Header.swift
+++ ScreenTimes/Network/Header.swift
@@ -7,8 +7,6 @@
 
 import Foundation
 
-import Foundation
-
 enum Header: String {
     case accept = "accept"
     case authorization = "Authorization"
--- ScreenTimes/Network/NetworkManager.swift
+++ ScreenTimes/Network/NetworkManager.swift
@@ -7,9 +7,29 @@
 
 import Foundation
 import Alamofire
+import RxSwift
 
 struct NetworkManager {
+    
     private init() { }
     
-
+    static func request<T: Decodable>(_ router: Router) -> Single<T> {
+        return Single.create { single in
+            do {
+                let request = try router.asURLRequest()
+                
+                AF.request(request).responseDecodable(of: T.self) { response in
+                    switch response.result {
+                    case .success(let value):
+                        single(.success(value))
+                    case .failure(let error):
+                        single(.failure(error))
+                    }
+                }
+            } catch {
+                single(.failure(error))
+            }
+            return Disposables.create()
+        }
+    }
 }
