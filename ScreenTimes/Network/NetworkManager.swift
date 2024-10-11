7e9ea328c427d9dbbe8871071ce9d7c5359a4ea2
skkim125 <kthanks125@gmail.com>
2024-10-11 15:27:37 +0900

    [Feat] SearchLogic(2)
    - SearchView에서 검색어 입력 시 검색 결과가 보여지도록 구현
    - 검색어가 비어있을 시 TrendMovie가 보여지도록 구현
    - CompostionalLayout의 threeCell case에 대해 셀 높이 소폭 수정

--- ScreenTimes/Network/NetworkManager.swift
+++ ScreenTimes/Network/NetworkManager.swift
@@ -13,20 +13,24 @@ struct NetworkManager {
     
     private init() { }
     
-    static func request<T: Decodable>(_ router: Router) -> Single<T> {
+    static func request<T: Decodable>(router: Router, model: T.Type) -> Single<T?> {
+        
         return Single.create { single in
             do {
                 let request = try router.asURLRequest()
-                AF.request(request).responseDecodable(of: T.self) { response in
-                    switch response.result {
-                    case .success(let value):
-                        single(.success(value))
-                    case .failure(let error):
-                        single(.failure(error))
+                AF.request(request)
+                    .responseDecodable(of: T.self) { response in
+                        switch response.result {
+                            
+                        case .success(let value):
+                            single(.success(value))
+                        case .failure(_):
+                            single(.success(nil))
+                        }
                     }
-                }
+                
             } catch {
-                single(.failure(error))
+                single(.success(nil))
             }
             return Disposables.create()
         }
--- ScreenTimes/View/VC/SearchViewController.swift
+++ ScreenTimes/View/VC/SearchViewController.swift
@@ -31,12 +31,14 @@ final class SearchViewController: BaseViewController {
     
     override func bind() {
         let dummy = PublishSubject<[MovieResult]>()
-        let trendArray = PublishSubject<[MovieResult]>()
-        let searchArray = PublishSubject<[MovieResult]>()
+        var trendArray: [MovieResult] = []
+        let setTrend = PublishSubject<Void>()
+        let setSearch = PublishSubject<String>()
         
-        NetworkManager.request(.trendingMovie)
-            .subscribe { (trend: TrendingMovie) in
-                trendArray.onNext(trend.results)
+        NetworkManager.request(router: .trendingMovie, model: TrendingMovie.self)
+            .subscribe { trend in
+                guard let trend = trend else { return }
+                trendArray = trend.results
                 dummy.onNext(trend.results)
             } onFailure: { error in
                 print(error)
@@ -57,9 +59,36 @@ final class SearchViewController: BaseViewController {
             .bind(with: self) { owner, text in
                 if text.trimmingCharacters(in: .whitespaces).isEmpty {
                     self.searchView.collectionView.rx.collectionViewLayout.onNext(owner.searchView.defaultCollectionViewLayout(.table))
+                    setTrend.onNext(())
                 } else {
-                    owner.searchView.collectionView.rx.collectionViewLayout.onNext(owner.searchView.defaultCollectionViewLayout(.threeCell))
+                    setSearch.onNext(text)
                 }
+            }
+            .disposed(by: disposeBag)
+        
+        setSearch
+            .bind(with: self) { owner, value in
+                NetworkManager.request(router: .searchMovie(query: value, page: 1), model: SearchMovie.self)
+                    .subscribe { search  in
+                        guard let search = search else { return }
+                        dummy.onNext(search.results)
+                        owner.searchView.rx.layoutType.onNext(.threeCell)
+                        owner.searchView.collectionView.rx.collectionViewLayout.onNext(owner.searchView.defaultCollectionViewLayout(.threeCell))
+                    } onFailure: { error in
+                        print(error)
+                    }
+                    .disposed(by: owner.disposeBag)
+                    
+            }
+            .disposed(by: disposeBag)
+        
+        
+        setTrend
+            .map({ trendArray })
+            .bind(with: self) { owner, value in
+                dummy.onNext(value)
+                owner.searchView.rx.layoutType.onNext(.table)
+                owner.searchView.collectionView.rx.collectionViewLayout.onNext(owner.searchView.defaultCollectionViewLayout(.table))
             }
             .disposed(by: disposeBag)
     }
