b4748dbf4d3e5963d9eb0fbaf5ac5031873b9caf
skkim125 <kthanks125@gmail.com>
2024-10-12 20:36:36 +0900

    [Feat] DetailView에서 사용할 Detail 타입 생성

---
+++ ScreenTimes/Model/DTO/Detail.swift
@@ -0,0 +1,19 @@
+//
+//  Detail.swift
+//  ScreenTimes
+//
+//  Created by 김상규 on 10/12/24.
+//
+
+import Foundation
+
+struct Detail {
+    let backdrop_path: String?
+    let id: Int
+    let name: String
+    let overview: String
+    let poster_path: String?
+    let media_type: String
+    let genre_ids: [Int]
+    let vote_average: Double
+}
